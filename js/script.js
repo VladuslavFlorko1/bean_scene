28b33449476f33d580c7560b32dfe9208aa055f0
VladuslavFlorko1 <florkovlad@gmail.com>
2026-06-01 19:13:50 +0300

    add sign in modal and modal toggle functionality

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const openSignUpButton = document.querySelector('.button-sign-up');
+const closeBtn = document.querySelector('.close-button');
+const closeBtnSignIn = document.querySelector('.close-button-sign-in');
+const modalWindow = document.querySelector('.modal-window-container');
+const openSignInButton = document.querySelector('.button-sign-in');
+const modalWindowSignIn = document.querySelector('.modal-window-container-sign-in');
+
+openSignInButton.addEventListener('click', () => {
+    modalWindowSignIn.classList.add('is-active-sign-in');
+});
+
+openSignUpButton.addEventListener('click', () => {
+    modalWindow.classList.add('is-active');
+});
+
+closeBtn.addEventListener('click', () => {
+    modalWindow.classList.remove('is-active');
+});
+
+closeBtnSignIn.addEventListener('click', () => {
+    modalWindowSignIn.classList.remove('is-active-sign-in');
+});
